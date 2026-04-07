54068ee6d5706b1120085099fda95a4640dfe7a5
anjali9472 <anjalisingh947299@gmail.com>
2026-04-07 19:26:10 +0530

    update backend url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
-import BestSeller from '../components/bestseller'
+import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
 import Newsletter from '../assets/Newsletter'
 
