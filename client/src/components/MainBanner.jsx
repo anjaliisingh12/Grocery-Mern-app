0c21d1d889476b697a4cba4d826d01f3d28b27bc
anjaliisingh12 <anjalisingh947299@gmail.com>
2026-05-07 16:35:19 +0530

    Refactor MainBanner component layout and styles

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -4,26 +4,34 @@ import { assets } from '../assets/assets.js';
 
 const MainBanner = () => {
   return (
-    <div className="relative">
+    <div className="relative h-[220px] md:h-[500px] overflow-hidden">
 
       <picture>
-        <source media="(max-width: 768px)" srcSet={assets.main_banner_bg_sm} />
+        <source
+          media="(max-width: 768px)"
+          srcSet={assets.main_banner_bg_sm}
+        />
+
         <img
           src={assets.main_banner_bg}
           alt="banner"
-          className="w-full"
-          fetchpriority="high"
+          className="w-full h-full object-cover"
           width="1200"
           height="500"
+          loading="eager"
+          fetchPriority="high"
+          decoding="async"
         />
       </picture>
 
       <div className="absolute inset-0 flex flex-col items-start justify-end md:justify-center pb-24 md:pb-0 px-4">
+
         <h1 className="text-xl md:text-4xl lg:text-5xl font-bold leading-tight max-w-72 md:max-w-xs">
           Freshness you can Trust, Savings you will love!
         </h1>
 
         <div className="flex items-center mt-6 font-medium">
+
           <Link
             to="/products"
             className="group flex items-center gap-2 py-3 px-7 text-white bg-orange-300 hover:bg-orange-500 transition rounded"
@@ -35,6 +43,7 @@ const MainBanner = () => {
               height="20"
               loading="lazy"
             />
+
             <span>Shop now</span>
           </Link>
 
@@ -43,6 +52,7 @@ const MainBanner = () => {
             className="group flex items-center gap-2 py-3 ml-4"
           >
             <span>Explore deals</span>
+
             <img
               src={assets.black_arrow_icon}
               alt="arrow"
@@ -51,6 +61,7 @@ const MainBanner = () => {
               loading="lazy"
             />
           </Link>
+
         </div>
       </div>
     </div>
